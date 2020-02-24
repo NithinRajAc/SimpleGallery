3dac3e4118fac8f08a0c7fd62936ed1032a128b4
NITHIN RAJ <nithinrajac@gmail.com>
2020-02-24 21:40:38 +0530

    Created adapter ImageListAdapter for recyclerview to show list of images.keeeping a datalist in adapter of type Imageitem.

---
+++ app/src/main/java/com/nr/simplegallery/adapters/ImageListAdapter.java
@@ -0,0 +1,76 @@
+package com.nr.simplegallery.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.nr.simplegallery.R;
+import com.nr.simplegallery.model.ImageItem;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ImageListAdapter extends RecyclerView.Adapter<ImageListAdapter.ViewHolder> {
+
+    private List<ImageItem> dataList;
+    private Context context;
+
+    public ImageListAdapter(Context context) {
+        this.context = context;
+        this.dataList = new ArrayList<>();
+    }
+
+    public ImageListAdapter setList(JSONArray array) {           // adding images items into datalist
+
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.optJSONObject(i);
+            ImageItem item = new ImageItem(object);
+            dataList.add(item);
+        }
+        notifyDataSetChanged();
+        return this;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_image_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.setData(dataList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageIV;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageIV = (ImageView) itemView.findViewById(R.id.imageIV);
+        }
+
+        void setData(ImageItem item) {                              //seting data for view holder
+
+            Glide.with(context)
+                    .load(item.getUrl())
+                    .centerCrop()
+                    .into(imageIV);
+        }
+    }
+}
---
+++ app/src/main/java/com/nr/simplegallery/model/ImageItem.java
@@ -0,0 +1,93 @@
+
+package com.nr.simplegallery.model;
+
+import org.json.JSONObject;
+
+public class ImageItem {
+
+    private String copyright;
+    private String date;
+    private String explanation;
+    private String hdurl;
+    private String mediaType;
+    private String serviceVersion;
+    private String title;
+    private String url;
+
+    public ImageItem(JSONObject object) {
+
+        this.copyright = object.optString("copyright");
+        this.date = object.optString("date");
+        this.explanation = object.optString("explanation");
+        this.hdurl = object.optString("hdurl");
+        this.mediaType = object.optString("mediaType");
+        this.serviceVersion = object.optString("serviceVersion");
+        this.title = object.optString("title");
+        this.url = object.optString("url");
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public String getServiceVersion() {
+        return serviceVersion;
+    }
+
+    public void setServiceVersion(String serviceVersion) {
+        this.serviceVersion = serviceVersion;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
